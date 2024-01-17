305badb80697ff105951c0a4d6b506f0377691fc
2011shao <562797023@qq.com>
2024-01-17 21:11:49 +0800

    去掉中文

--- ytb.py
+++ ytb.py
@@ -1,23 +1,13 @@
 import os
 from googleapiclient.discovery import build
-# 设置API密钥
 api_key = 'AIzaSyDki3M1XufmBDy8Vq-87VCNpo6OUCKk91M'
-# //os.environ['YOUTOBE']
-# 创建YouTube API服务
 youtube = build('youtube', 'v3', developerKey=api_key)
-
-# 测试示例
 url = "https://www.youtube.com/watch?v=VIDEO_ID"
-
-
 def get_video_info(video_url, is_commentl):
   try:
     if video_url is None or ('youtube.com' not in video_url):
       return {"code": 1, "errMsg": "video url error"}
-    # 从视频链接中提取视频ID
     video_id = video_url.split('=')[1]
-    # 你的 API 调用代码
-    # 使用视频ID获取视频信息
     video_info = youtube.videos().list(part='snippet,statistics',
                                        id=video_id).execute()
     comments = []
@@ -36,9 +26,6 @@ def get_video_info(video_url, is_commentl):
             'comment_time': comment['publishedAt']
         }
         comments.append(comment_info)
-
-    print("视频评论:", comments)
-
     return {"code": 0, "errMsg": "", "data": video_info, "comments": comments}
 
   except Exception as e:
