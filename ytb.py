8a91ee2705dff408de8aa6e018ad2f860e078f20
2011shao <562797023@qq.com>
2024-01-19 22:16:02 +0800

    兼容短视频链接

--- ytb.py
+++ ytb.py
@@ -1,13 +1,29 @@
 import os
 from googleapiclient.discovery import build
-api_key = 'AIzaSyDki3M1XufmBDy8Vq-87VCNpo6OUCKk91M'
+import re
+
+api_key = os.environ['YOUTOBE']
 youtube = build('youtube', 'v3', developerKey=api_key)
 url = "https://www.youtube.com/watch?v=VIDEO_ID"
+
+def extract_video_id(video_url):
+    # 使用正则表达式从视频链接中提取视频ID
+    video_id_match = re.search(r'[?&]v=([a-zA-Z0-9_-]+)', video_url)
+    if video_id_match:
+        return video_id_match.group(1)
+
+    # 如果是短链接，使用 /shorts/ 后的部分作为视频ID
+    shorts_match = re.search(r'/shorts/([a-zA-Z0-9_-]+)', video_url)
+    if shorts_match:
+        return shorts_match.group(1)
+
+    return None
 def get_video_info(video_url, is_commentl):
   try:
-    if video_url is None or ('youtube.com' not in video_url):
+    video_id = extract_video_id(video_url)
+
+    if not video_id:
       return {"code": 1, "errMsg": "video url error"}
-    video_id = video_url.split('=')[1]
     video_info = youtube.videos().list(part='snippet,statistics',
                                        id=video_id).execute()
     comments = []
